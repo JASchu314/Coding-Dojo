e4d211c800e4a4f60355f387111c974e2507a572
JASchu314 <34992687+JASchu314@users.noreply.github.com>
2018-02-11 18:50:45 -0800

    Dojo Survey with Validation
    added validation to basic Dojo Survey

--- dojosurvey/server.py
+++ dojosurvey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session , flash
 
 app = Flask(__name__)
 app.secret_key = "This is hidden"
@@ -8,9 +8,19 @@ def form():
     return render_template("index.html")
 @app.route('/users', methods=['POST'])
 def returnform():
-    session['yourname'] = request.form['yourname']
-    session['Dojo-Location'] = request.form["Dojo-Location"]
-    session['Favorite-Language'] = request.form['Favorite-Language']
-    session['comment'] = request.form['comment']
-    return render_template('users.html')
+    if len(request.form['yourname']) < 1:
+        flash('Name cannot be Empty!')
+        return redirect('/')
+    elif len(request.form['comment']) > 120:
+        flash('Comments cannot exceed 120 characters')
+        return redirect('/')
+    elif len(request.form['comment']) < 1:
+        flash('We need your feedback! Please Leave a Comment!')
+        return redirect('/')
+    else:
+        session['yourname'] = request.form['yourname']
+        session['Dojo-Location'] = request.form["Dojo-Location"]
+        session['Favorite-Language'] = request.form['Favorite-Language']
+        session['comment'] = request.form['comment']
+        return render_template('users.html')
 app.run(debug=True)
